9c61fcf45030eeff46c8334e5a28af2886f1095f
seohyun <amanda94@naver.com>
2023-08-14 18:38:54 +0900

    feat: 공통 버튼 및 themeProvider 추가

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+const colors = {
+    point1: '#8B97FF',
+    point2: '#A888FF',
+    sub1: '#D373FA',
+    sub2: '#FFC348',
+    sub3: '#FFA888',
+    black: '1B1D21',
+};
+
+const theme = {
+    colors,
+};
+
+export default theme;
