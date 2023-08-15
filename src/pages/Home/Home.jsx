85e62c077b860e448d7bd33b1c223d1b8c57bff1
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-16 04:42:54 +0900

    Feat: Home 페이지 UI 작성

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,43 @@
+import Badge from '../../components/Badge/Badge';
+import CategoryLink from '../../components/CategoryLink/CategoryLink';
+import * as H from './Home.styles';
+// {
+//     "artsyData": [
+//       {
+//         "name": "뮤지컬",
+//         "color": "#ff0000"
+//       }
+//     ]
+//   }
+const categories = [{ name: '뮤지컬' }, { name: '영화' }, { name: '전시' }, { name: '콘서트' }, { name: '연극' }];
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <H.Home>
+            <H.UserInfo>
+                <H.MainGreeting>
+                    <h2>
+                        안녕하세요,
+                        <br />
+                        서현님
+                    </h2>
+                    <p>오늘의 추억이 있으신가요?</p>
+                </H.MainGreeting>
+                <Badge />
+            </H.UserInfo>
+            <H.RecordCollection>
+                <H.Record>
+                    <h2>기록함</h2>
+                    <p>나만의 공간에 오늘의 소중한 문화 경험을 기록해보세요!</p>
+                </H.Record>
+                <H.CategoryContainer>
+                    <H.CategoryWrapper>
+                        {categories.map((cat, index) => (
+                            <CategoryLink name={cat.name} key={index} />
+                        ))}
+                    </H.CategoryWrapper>
+                </H.CategoryContainer>
+            </H.RecordCollection>
+        </H.Home>
+    );
 };
 export default Home;
--- src/pages/Home/Home.styles.js
+++ src/pages/Home/Home.styles.js
@@ -1,3 +1,96 @@
 import styled from 'styled-components';
+const Home = styled.div`
+    width: 100%;
+    @media all and (min-width: 768px) {
+        max-width: 1280px;
+        margin: 0 auto;
+    }
+`;
 
-export {};
+const UserInfo = styled.div`
+    padding: 20px;
+    margin-bottom: 4%;
+    display: flex;
+    flex-direction: column;
+    @media all and (min-width: 768px) {
+        flex-direction: row;
+        margin-bottom: 2%;
+        padding: 0 20px;
+    }
+`;
+const MainGreeting = styled.div`
+    margin-bottom: 6%;
+    h2 {
+        font-size: 1.3rem;
+        margin-bottom: 4%;
+        font-weight: 500;
+        line-height: 1.8rem;
+    }
+    p {
+        display: none;
+    }
+    @media all and (min-width: 768px) {
+        flex: 1 auto;
+        h2 {
+            font-size: 1.5rem;
+            font-weight: bold;
+            line-height: 2rem;
+        }
+        p {
+            display: block;
+        }
+    }
+`;
+const RecordCollection = styled.div`
+    position: relative;
+`;
+const Record = styled.div`
+    padding: 0 20px;
+
+    h2 {
+        font-size: 1.3rem;
+        font-weight: 600;
+        margin-bottom: 3%;
+    }
+    p {
+        position: absolute;
+        font-size: 0.7rem;
+    }
+    @media all and (min-width: 768px) {
+        h2 {
+            font-size: 1.5rem;
+            font-weight: bold;
+            margin-bottom: 1%;
+        }
+        p {
+            font-size: 0.9rem;
+        }
+    }
+`;
+const CategoryContainer = styled.div`
+    width: 100%;
+
+    scroll-snap-type: x mandatory;
+    overflow: auto;
+    @media all and (min-width: 768px) {
+        scroll-snap-type: none;
+    }
+`;
+
+const CategoryWrapper = styled.div`
+    display: flex;
+    width: 200vw;
+    padding: 8% 0 12%;
+    margin-left: 20px;
+    div {
+        scroll-snap-align: center;
+        scroll-snap-stop: normal;
+    }
+    @media all and (min-width: 768px) {
+        width: 100%;
+        box-sizing: border-box;
+        margin-left: 0;
+        padding: 4% 0 12% 3%;
+    }
+`;
+export { Home, CategoryContainer, CategoryWrapper, RecordCollection, UserInfo, Record, MainGreeting };
