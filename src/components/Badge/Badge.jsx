0cdc40ca877cca9a1d0a1bcd95e00c03cae86e71
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-16 16:48:19 +0900

    Feat: 뱃지 등급을 계산하는 기능 추가

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import * as B from './Badge.styles';
 import { useTheme } from 'styled-components';
+import calculateGrade from '../../utils/calculateGrade';
 
 const data = {
     ticketCount: 4,
-    currentBadge: { name: 'silver', standard: 10 },
-    nextBadge: { name: 'bronze', standard: 30 },
+    // currentBadge: { name: 'silver', standard: 10 },
+    // nextBadge: { name: 'bronze', standard: 30 },
 };
+
 const Badge = () => {
     const theme = useTheme();
+    const { name: userGrade, targetValue } = calculateGrade(data.ticketCount);
     return (
         <B.Badge theme={theme}>
             <B.ImageWrapper>
-                <img src={`/src/assets/icons/badge_${data.currentBadge.name}.png`} alt="badge image" />
+                <img src={`/src/assets/icons/badge_${userGrade}.png`} alt="badge image" />
             </B.ImageWrapper>
             <B.Description>
                 다음 뱃지까지 <br />
-                6개의 기록이 남았습니다!
+                {targetValue - data.ticketCount}개의 기록이 남았습니다!
             </B.Description>
             <B.BadgeValue theme={theme}>
-                {data.ticketCount} <span>/ {data.currentBadge.standard}</span>
+                {data.ticketCount} <span>/ {targetValue}</span>
             </B.BadgeValue>
             <B.Bar>
-                <B.Value $all={data.currentBadge.standard} $value={data.ticketCount} theme={theme}></B.Value>
+                <B.Value $all={targetValue} $value={data.ticketCount} theme={theme}></B.Value>
             </B.Bar>
         </B.Badge>
     );
