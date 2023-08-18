f4d565d5f0526d0fcb2216dcec02628740e8b20f
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-18 11:20:18 +0900

    Feat. 티켓 요청 테스트 추가

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from 'react';
+import axiosInstance from '../../api';
+import useLoginQuery from '../../hooks/@queries/useLoginQuery';
+
 const Home = () => {
+    const [tickets, setTickets] = useState({});
+    useEffect(() => {
+        axiosInstance
+            .get('/api/user/ticket-total-count', {
+                headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+            })
+            .then((res) => res.data.message)
+            .then(setTickets)
+            .then(() => console.log(tickets));
+    }, []);
+
     return <h1>Home</h1>;
 };
 export default Home;
