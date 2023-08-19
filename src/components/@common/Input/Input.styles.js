fb49dffaafbbe726b034021bd57121b6ff05c0b3
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-19 17:38:09 +0900

    Fix: id props를 label과 input에서 쓰도록 수정, 스타일컴포넌트명 변경

--- src/components/@common/Input/Input.styles.js
+++ src/components/@common/Input/Input.styles.js
@@ -1,19 +1,19 @@
 import styled from 'styled-components';
 
-const FormGroup = styled.div`
+const StyledForm = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
 `;
-const Label = styled.label`
+const StyledLabel = styled.label`
     font-size: 18px;
     font-weight: bold;
     margin-bottom: 16px;
 `;
-const Required = styled.span`
+const StyledRequired = styled.span`
     color: #ff1f00;
 `;
-const Input = styled.input`
+const StyledInput = styled.input`
     box-sizing: border-box;
     width: ${(props) => props.$inputWidth};
     height: 54px;
@@ -26,8 +26,8 @@ const Input = styled.input`
     border-color: ${(props) => (props.$valid ? '#c5c5c5' : '#FF1F00')};
 `;
 
-const ErrorMessage = styled.span`
+const StyledErrorMessage = styled.span`
     color: #ff1f00;
 `;
 
-export { FormGroup, Input, Label, Required, ErrorMessage };
+export { StyledForm, StyledInput, StyledLabel, StyledRequired, StyledErrorMessage };
