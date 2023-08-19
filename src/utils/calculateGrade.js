07064a1e10ff8cde81ccf6bd050312a43f22af21
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-19 20:04:28 +0900

    Feat: 뱃지 등급의 이름 수정, 등급 계산 함수 수정

--- src/utils/calculateGrade.js
+++ src/utils/calculateGrade.js
@@ -1,19 +1,12 @@
-const BADGE_GRADE = {
-    first: { name: 'darkSilver', targetValue: 10 },
-    second: { name: 'bronze', targetValue: 30 },
-    third: { name: 'silver', targetValue: 50 },
-    fourth: { name: 'gold', targetValue: 100 },
+const BADGE_GRADE = [
+    { name: '아티 비기너', targetValue: 10, imageName: 'darkSilver' },
+    { name: '아티 프렌즈', targetValue: 30, imageName: 'bronze' },
+    { name: '아티 패밀리', targetValue: 70, imageName: 'silver' },
+    { name: '아티 마스터', targetValue: Number.POSITIVE_INFINITY, imageName: 'gold' },
+];
+const calculateGrade = (ticketCount) => {
+    const selectedGrade = BADGE_GRADE.find(({ targetValue }) => ticketCount < targetValue);
+    return selectedGrade;
 };
 
-const calculateGrade = (ticketCount) => {
-    if (ticketCount < BADGE_GRADE.first.targetValue) {
-        return BADGE_GRADE.first;
-    } else if (ticketCount < BADGE_GRADE.second.targetValue) {
-        return BADGE_GRADE.second;
-    } else if (ticketCount < BADGE_GRADE.third.targetValue) {
-        return BADGE_GRADE.third;
-    } else {
-        return BADGE_GRADE.fourth;
-    }
-};
 export default calculateGrade;
