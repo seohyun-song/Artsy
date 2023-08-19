a9dfb543bf6cd4e1d912268e778fc47a3b05fbd1
seohyun <amanda94@naver.com>
2023-08-20 00:16:07 +0900

    chore: 등급정책 변경에따른 수정

--- src/components/@common/GradeBar/GradeBar.jsx
+++ src/components/@common/GradeBar/GradeBar.jsx
@@ -1,24 +1,17 @@
 import { StyledBarWrap, StyledBar, StyledLegend } from './GradeBar.styles';
 
 /* 지영님 코드 병합시 impor 구문으로 변경 */
-const BADGE_GRADE = {
-    first: { name: 'darkSilver', targetValue: 10 },
-    second: { name: 'bronze', targetValue: 30 },
-    third: { name: 'silver', targetValue: 50 },
-    fourth: { name: 'gold', targetValue: 100 },
+const BADGE_GRADE = [
+    { name: '아티 비기너', targetValue: 10, imageName: 'darkSilver' },
+    { name: '아티 프렌즈', targetValue: 30, imageName: 'bronze' },
+    { name: '아티 패밀리', targetValue: 70, imageName: 'silver' },
+    { name: '아티 마스터', targetValue: Number.POSITIVE_INFINITY, imageName: 'gold' },
+];
+const calculateGrade = (ticketCount) => {
+    const selectedGrade = BADGE_GRADE.find(({ targetValue }) => ticketCount < targetValue);
+    return selectedGrade;
 };
 
-const calculateGrade = (ticketCount) => {
-    if (ticketCount < BADGE_GRADE.first.targetValue) {
-        return BADGE_GRADE.first;
-    } else if (ticketCount < BADGE_GRADE.second.targetValue) {
-        return BADGE_GRADE.second;
-    } else if (ticketCount < BADGE_GRADE.third.targetValue) {
-        return BADGE_GRADE.third;
-    } else {
-        return BADGE_GRADE.fourth;
-    }
-};
 /* // 지영님 코드 병합시 impor 구문으로 변경 */
 
 /**
@@ -29,16 +22,19 @@ const calculateGrade = (ticketCount) => {
  * @returns
  */
 const GradeBar = ({ total, width, height }) => {
-    const { name, targetValue } = calculateGrade(total);
-    const percent = Math.floor((total / targetValue) * 100);
+    const { targetValue, imageName } = calculateGrade(total);
+    let percent = 0;
+    if (total !== 0) {
+        percent = total >= 70 ? 100 : Math.floor((total / targetValue) * 100);
+    }
 
     return (
         <StyledBarWrap $width={width}>
-            <StyledLegend $grade={name}>
+            <StyledLegend $grade={imageName}>
                 <span>{total}</span>
                 <span> / {targetValue}</span>
             </StyledLegend>
-            <StyledBar $grade={name} $percent={percent ?? 0} $height={height} />
+            <StyledBar $grade={imageName} $percent={percent ?? 0} $height={height} />
         </StyledBarWrap>
     );
 };
