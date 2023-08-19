ac82019c8cec1f2d52ed1b79bb47007939b18603
seohyun <amanda94@naver.com>
2023-08-20 00:16:45 +0900

    feat: 마이페이지 api 데이터 작업중

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,9 +1,13 @@
 import Container from '@components/@common/Container/Container.jsx';
 import GradeBar from '@components/@common/GradeBar/GradeBar.jsx';
+import Loading from '@components/@common/Loading/Loading.jsx';
 
 import MyGreeting from '@components/MyPage/MyGreeting/MyGreeting.jsx';
 import MyIconButton from '@components/MyPage/MyIconButton/MyIconButton.jsx';
 import MyExpense from '@components/MyPage/MyExpense/MyExpense.jsx';
+
+import useUserInfoQuery from '@hooks/@queries/useUserInfoQuery';
+import useTotalPriceQuery from '@hooks/@queries/useTotalPriceQuery';
 
 import myIconUrl from '@assets/icons/icon-my.png';
 import bookIconUrl from '@assets/icons/icon-book.png';
@@ -11,14 +15,32 @@ import chartIconUrl from '@assets/icons/icon-chart.png';
 
 import * as M from './MyPage.styles';
 
+/* 지영님 코드 병합시 impor 구문으로 변경 */
+const BADGE_GRADE = [
+    { name: '아티 비기너', targetValue: 10, imageName: 'darkSilver' },
+    { name: '아티 프렌즈', targetValue: 30, imageName: 'bronze' },
+    { name: '아티 패밀리', targetValue: 70, imageName: 'silver' },
+    { name: '아티 마스터', targetValue: Number.POSITIVE_INFINITY, imageName: 'gold' },
+];
+const calculateGrade = (ticketCount) => {
+    const selectedGrade = BADGE_GRADE.find(({ targetValue }) => ticketCount < targetValue);
+    return selectedGrade;
+};
+
+/* // 지영님 코드 병합시 impor 구문으로 변경 */
+
 const MyPage = () => {
+    const userInfoQuery = useUserInfoQuery();
+    const totalPriceQuery = useTotalPriceQuery();
+
+    if (userInfoQuery.isLoading || totalPriceQuery.isLoading) return <Loading></Loading>;
+
     return (
         <>
             <Container>
                 <M.MyPage>
                     <M.ViewWrap>
-                        {/* api 호출 정보 */}
-                        <MyGreeting username={'지영'} />
+                        <MyGreeting username={userInfoQuery?.data?.displayName} />
                         <M.IconMenuList>
                             <MyIconButton to="" imgUrl={myIconUrl}>
                                 계정정보
@@ -32,17 +54,20 @@ const MyPage = () => {
                         </M.IconMenuList>
                         <M.GradeBox>
                             <M.GradeBoxTop>
-                                {/* api 호출 정보 */}
-                                <h4>문화 전문가</h4>
-                                <p>문화 마스터까지 {13}개의 티켓이 필요해요!</p>
+                                <h4>{calculateGrade(userInfoQuery?.data?.totalTicket).name}</h4>
+                                <p>
+                                    문화 마스터까지{' '}
+                                    {calculateGrade(userInfoQuery?.data?.totalTicket).targetValue -
+                                        userInfoQuery?.data?.totalTicket}
+                                    개의 티켓이 필요해요!
+                                </p>
                             </M.GradeBoxTop>
-                            {/* api 호출 정보 */}
-                            <GradeBar total={30} height={0.8} />
+                            <GradeBar total={userInfoQuery?.data?.totalTicket} height={0.8} />
                         </M.GradeBox>
                     </M.ViewWrap>
                     <M.DataWrap>
                         {/* api 호출 정보 */}
-                        <MyExpense totalPrice={'125,000,000'} />
+                        <MyExpense totalPrice={totalPriceQuery?.data?.totalPrice.toLocaleString()} />
                     </M.DataWrap>
                 </M.MyPage>
             </Container>
