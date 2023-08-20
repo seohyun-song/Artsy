c06745433f3b67add0ca411f62088b74e73adfe1
[SW5] 김지윤 <eliceyunva17@gmail.com>
2023-08-20 16:13:37 +0900

    feat: 슬라이더바

---
+++ src/components/TicketCreate/RatingSliderBar/RatingSliderBar.styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const SliderWrap = styled.div`
+    width: 100%;
+    margin: 1rem 0;
+
+    div {
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    h5 {
+        font-size: 1.7rem;
+        color: ${({ theme }) => theme.colors.point2};
+        margin-bottom: 1rem;
+    }
+
+    input {
+        width: 100%;
+        accent-color: ${({ theme }) => theme.colors.point2};
+    }
+`;
+
+export { SliderWrap };
