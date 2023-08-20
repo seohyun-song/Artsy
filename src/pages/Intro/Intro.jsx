ce7cde85e86d829ba7837d90e86734ac9a3ab65a
seohyun <amanda94@naver.com>
2023-08-20 11:46:30 +0900

    feat: intro 작업중

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -1,4 +1,25 @@
+import Container from '@components/@common/Container/Container.jsx';
+import Button from '@components/@common/Button/Button.jsx';
+
+import * as I from './Intro.styles';
+
 const Intro = () => {
-    return <h1>Intro</h1>;
+    return (
+        <>
+            <Container>
+                <I.MainVisual>
+                    <I.MainText>
+                        <h2>
+                            보기만 해도 뿌듯한!
+                            <br /> 나만의 티켓북
+                        </h2>
+                        <p>이제 온라인으로 나만의 문화 티켓을 기록하세요.</p>
+
+                        <Button size="large">바로 시작하기</Button>
+                    </I.MainText>
+                </I.MainVisual>
+            </Container>
+        </>
+    );
 };
 export default Intro;
--- src/pages/Intro/Intro.styles.js
+++ src/pages/Intro/Intro.styles.js
@@ -1,3 +1,43 @@
 import styled from 'styled-components';
 
-export {};
+import introUrl from '@assets/images/img-intro.png';
+
+const MainVisual = styled.section`
+    position: relative;
+    height: 40vh;
+
+    &:before {
+        content: '';
+        z-index: -1;
+        position: absolute;
+        display: block;
+        width: 100%;
+        height: 100%;
+        background: url(${introUrl}) no-repeat center / cover;
+        filter: blur(8px);
+        -webkit-filter: blur(0.8px);
+    }
+`;
+
+const MainText = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    height: 100%;
+    padding: 2rem;
+    box-sizing: border-box;
+
+    h2 {
+        font-size: ${({ theme }) => theme.fontSizes.h5};
+    }
+    p {
+        color: #909090;
+        font-size: ${({ theme }) => theme.fontSizes.text};
+    }
+
+    button {
+        align-self: center;
+        margin-top: auto;
+    }
+`;
+export { MainVisual, MainText };
