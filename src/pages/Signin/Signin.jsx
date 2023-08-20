22652fde00f9aec1864f480b84e730eabbc1aa1b
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-20 17:54:08 +0900

    Feat: SignIn 페이지에 GlobalStyle 적용

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import SplitLayout from '@components/SplitLayout/SplitLayout';
 import { ERROR_MESSAGE } from '@constants/message';
 import { VALIDATE } from '@constants/regexp';
+import GlobalStyle from '@styles/GlobalStyles';
 
 function checkValidEmail(email) {
     var pattern = VALIDATE.email;
@@ -52,48 +53,51 @@ const Signin = () => {
         []
     );
     return (
-        <SplitLayout leftCss={leftBgColor}>
-            <S.SignInContainer>
-                <S.Introduction $theme={theme}>
-                    <h2>Artsy</h2>
-                    <p>나만의 티켓북을 만들고 관리하는 서비스를 경험해보세요!</p>
-                </S.Introduction>
-                <S.SignInContent $theme={theme}>
-                    <S.loginTitle>Welcome!</S.loginTitle>
-                    <S.loginForm onSubmit={handleSubmit} $theme={theme}>
-                        <Input
-                            placeholder="이메일 주소를 입력하세요."
-                            id="email"
-                            name="email"
-                            onChange={handleChange}
-                            value={loginInfo.email}
-                            rounded
-                            isRequired
-                            isValid={true}
-                            inputWidth="100%"
-                        />
-                        <Input
-                            inputType="password"
-                            placeholder="비밀번호를 입력하세요."
-                            id="password"
-                            name="password"
-                            onChange={handleChange}
-                            value={loginInfo.password}
-                            rounded
-                            isRequired
-                            isValid={true}
-                            inputWidth="100%"
-                        />
-                        <S.SignInButton color={theme.colors.point1} size={'large'}>
-                            로그인
-                        </S.SignInButton>
-                    </S.loginForm>
-                    <S.RegisterButton $theme={theme} size={'large'} onClick={handleRegister}>
-                        회원가입
-                    </S.RegisterButton>
-                </S.SignInContent>
-            </S.SignInContainer>
-        </SplitLayout>
+        <>
+            <GlobalStyle />
+            <SplitLayout leftCss={leftBgColor}>
+                <S.SignInContainer>
+                    <S.Introduction $theme={theme}>
+                        <h2>Artsy</h2>
+                        <p>나만의 티켓북을 만들고 관리하는 서비스를 경험해보세요!</p>
+                    </S.Introduction>
+                    <S.SignInContent $theme={theme}>
+                        <S.loginTitle>Welcome!</S.loginTitle>
+                        <S.loginForm onSubmit={handleSubmit} $theme={theme}>
+                            <Input
+                                placeholder="이메일 주소를 입력하세요."
+                                id="email"
+                                name="email"
+                                onChange={handleChange}
+                                value={loginInfo.email}
+                                rounded
+                                isRequired
+                                isValid={true}
+                                inputWidth="100%"
+                            />
+                            <Input
+                                inputType="password"
+                                placeholder="비밀번호를 입력하세요."
+                                id="password"
+                                name="password"
+                                onChange={handleChange}
+                                value={loginInfo.password}
+                                rounded
+                                isRequired
+                                isValid={true}
+                                inputWidth="100%"
+                            />
+                            <S.SignInButton color={theme.colors.point1} size={'large'}>
+                                로그인
+                            </S.SignInButton>
+                        </S.loginForm>
+                        <S.RegisterButton $theme={theme} size={'large'} onClick={handleRegister}>
+                            회원가입
+                        </S.RegisterButton>
+                    </S.SignInContent>
+                </S.SignInContainer>
+            </SplitLayout>
+        </>
     );
 };
 export default Signin;
