9c3d86f102359f6937bbed21740c0a1c158a641f
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-20 17:53:08 +0900

    Feat: BADGE_GRADE를 상수 디렉터리에서 import하도록 수정

--- src/utils/calculateGrade.js
+++ src/utils/calculateGrade.js
@@ -1,9 +1,5 @@
-const BADGE_GRADE = [
-    { name: '아티 비기너', targetValue: 10, imageName: 'darkSilver' },
-    { name: '아티 프렌즈', targetValue: 30, imageName: 'bronze' },
-    { name: '아티 패밀리', targetValue: 70, imageName: 'silver' },
-    { name: '아티 마스터', targetValue: Number.POSITIVE_INFINITY, imageName: 'gold' },
-];
+import { BADGE_GRADE } from '@constants/badgeGrade';
+
 const calculateGrade = (ticketCount) => {
     const selectedGrade = BADGE_GRADE.find(({ targetValue }) => ticketCount < targetValue);
     return selectedGrade;
