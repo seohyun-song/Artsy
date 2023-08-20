b9bbba88efe815f52cb0b1bffbfc118e330c8b81
seohyun <amanda94@naver.com>
2023-08-20 11:04:50 +0900

    chore: 뱃지등급 상수모듈 사용

--- src/components/@common/GradeBar/GradeBar.jsx
+++ src/components/@common/GradeBar/GradeBar.jsx
@@ -1,12 +1,6 @@
 import { StyledBarWrap, StyledBar, StyledLegend } from './GradeBar.styles';
+import { BADGE_GRADE } from '@constants/badgeGrade';
 
-/* 지영님 코드 병합시 impor 구문으로 변경 */
-const BADGE_GRADE = [
-    { name: '아티 비기너', targetValue: 10, imageName: 'darkSilver' },
-    { name: '아티 프렌즈', targetValue: 30, imageName: 'bronze' },
-    { name: '아티 패밀리', targetValue: 70, imageName: 'silver' },
-    { name: '아티 마스터', targetValue: Number.POSITIVE_INFINITY, imageName: 'gold' },
-];
 const calculateGrade = (ticketCount) => {
     const selectedGrade = BADGE_GRADE.find(({ targetValue }) => ticketCount < targetValue);
     return selectedGrade;
@@ -25,7 +19,7 @@ const GradeBar = ({ total, width, height }) => {
     const { targetValue, imageName } = calculateGrade(total);
     let percent = 0;
     if (total !== 0) {
-        percent = total >= 70 ? 100 : Math.floor((total / targetValue) * 100);
+        percent = total >= BADGE_GRADE[2].targetValue ? 100 : Math.floor((total / targetValue) * 100);
     }
 
     return (
