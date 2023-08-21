feb55d26bb4d13087a2dc732afdd442577258185
seohyun <amanda94@naver.com>
2023-08-21 20:59:57 +0900

    chore: 이미지 경로 env환경정보사용한 동적생성

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,12 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
     esbuild: {},
+    define: {
+        'process.env': process.env,
+    },
     plugins: [react()],
     resolve: {
         alias: [
-            // find: 별칭, replacement: 절대 경로 주입
             { find: '@', replacement: path.resolve(__dirname, 'src') },
             {
                 find: '@api',
