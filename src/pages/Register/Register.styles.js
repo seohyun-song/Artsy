bb11014b7eea0670b335313faf90a5bd250d9de3
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-21 19:06:31 +0900

    Design: input 너비와 폰트 사이즈 조정

--- src/pages/Register/Register.styles.js
+++ src/pages/Register/Register.styles.js
@@ -11,24 +11,24 @@ const RegisterContainer = styled.div`
     justify-content: center;
     align-items: center;
 
+    input {
+        font-size: 1.4rem;
+    }
     ${({ $theme }) => $theme.media.labtop} {
         flex-basis: 40%;
     }
 `;
 const Title = styled.h2`
+    font-size: 3.4rem;
     margin-bottom: 2.6rem;
 `;
 const RegisterForm = styled.form`
     width: 100%;
-    max-width: 480px;
-
+    max-width: 290px;
     display: grid;
     /* grid-template-rows: 57px 57px 53px; */
     grid-gap: 12px;
     margin-bottom: 16px;
-    ${({ $theme }) => $theme.media.labtop} {
-        max-width: 370px;
-    }
 `;
 const RegisterButton = styled(Button)`
     border-radius: 100px;
