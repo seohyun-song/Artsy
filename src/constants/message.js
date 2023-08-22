a661fc1f007a33c33d9bb8b7a58e8063dc387b20
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-22 22:48:39 +0900

    Fix: 에러 메시지 수정

--- src/constants/message.js
+++ src/constants/message.js
@@ -5,8 +5,7 @@ const CONFIRM_MESSAGE = {
 const ERROR_MESSAGE = {
     unexpected: '앗! 오류가 발생했어요.' + '\n' + '잠시 후에 다시 시도해보세요.',
     auth: '로그인이 필요한 페이지입니다. 로그인을 먼저 해주세요.',
-    incorrectEmailOrPassword: `이메일 주소 또는 비밀번호를 잘못입력했습니다.
-    다시 확인해주세요.`,
+    incorrectEmailOrPassword: `이메일 주소 또는 비밀번호를 잘못입력했습니다. 다시 확인해주세요.`,
     incorrectEmailFormat: '이메일 형식이 올바르지 않습니다.',
     duplicatedEmail: '이미 사용 중인 이메일 주소입니다.',
 };
