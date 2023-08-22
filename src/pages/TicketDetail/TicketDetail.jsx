e334e26c21adca5d14d9eee06bee9be674f1c2ff
[SW5] 김지윤 <eliceyunva17@gmail.com>
2023-08-22 23:43:47 +0900

    feat:  티켓 상세 api 연결

--- src/pages/TicketDetail/TicketDetail.jsx
+++ src/pages/TicketDetail/TicketDetail.jsx
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as T from './TicketDetail.styles';
 import DetailText from '@components/TicketDetail/DetailText';
 import { useParams } from 'react-router-dom';
+import BasicTicketUrl from '@assets/images/ticket.png';
+import { useTicketGetQuery } from '@hooks/@queries/useTicketQuery';
+import Loading from '@components/@common/Loading/Loading';
+import formatKstDate from '@utils/foramtKstDate';
+import formatTicketDate from '@utils/formatTicketDate';
+import { ERROR_TYPE } from '@constants/serverErrorType';
+import { ERROR_MESSAGE } from '@constants/message';
 
 const TicketDetail = () => {
     const { ticketId } = useParams();
+    const ticketQuery = useTicketGetQuery(ticketId);
+    const ticketData = ticketQuery?.data;
 
-    const imgUrl =
-        'https://i.namu.wiki/i/CM9WgqYNFXtGFZCtBU1r2Exs1y-zKyjmIW55gBudgExj9Q6NIfUavAeq7Tn55FB-GxyJ8hWK9PShcQVBdxJPwQ.webp';
+    const [imgSrc, setImgSrc] = useState('');
 
-    const ticketData = {
-        categoryName: '영화',
-        title: '엘리멘탈',
-        showDate: '2023-08-14',
-        rating: '4',
-        place: '성수 메가박스',
-        price: 12000,
-        review: '오늘 영화 봄',
-        categoryColor: '#A888FF',
-    };
+    useEffect(() => {
+        if (ticketQuery.isSuccess) {
+            if (ticketData.files?.length === 0) {
+                setImgSrc(BasicTicketUrl);
+            } else {
+                setImgSrc(ticketData.files[0].imageUrl);
+            }
+        }
+    }, [ticketQuery.isSuccess]);
 
+    useEffect(() => {
+        if (ticketQuery.isError) {
+            const errorType = query.error.response.data.error.type;
+            switch (errorType) {
+                case ERROR_TYPE.DATA_NOT_FOUND: {
+                    alert(ERROR_MESSAGE.dataNotFoundTicket);
+                    break;
+                }
+                default: {
+                    alert('관리자에게 문의하세요');
+                }
+            }
+        }
+    }, [ticketQuery.isError]);
+
+    if (ticketQuery.isLoading) return <Loading />;
     return (
         <>
             <T.TypeColorBox color={ticketData.categoryColor} />
@@ -29,7 +52,7 @@ const TicketDetail = () => {
                 </T.MainWrap>
                 <T.TicketDetailWrap>
                     <T.TicketImageWrap>
-                        <img src={imgUrl} alt="티켓이미지" />
+                        <img src={imgSrc} alt="티켓이미지" />
                     </T.TicketImageWrap>
                     <T.TicketDetailBox>
                         <T.DetailContainer>
@@ -40,10 +63,15 @@ const TicketDetail = () => {
                             <T.DetailWrap>
                                 <DetailText label="카테고리" content={ticketData.categoryName} />
                                 <DetailText label="별점" rating={ticketData.rating} />
-                                <DetailText label="장소" content={ticketData.place} />
-                                <DetailText label="금액" content={`${ticketData.price.toLocaleString()}원`} />
-                                <DetailText label="일시" content={ticketData.showDate} />
-                                <DetailText label="리뷰" content={ticketData.review} />
+                                {ticketData.place !== '' && <DetailText label="장소" content={ticketData.place} />}
+                                {ticketData.price !== 0 && (
+                                    <DetailText label="금액" content={`${ticketData.price.toLocaleString()}원`} />
+                                )}
+                                <DetailText
+                                    label="일시"
+                                    content={formatTicketDate(formatKstDate(ticketData.showDate))}
+                                />
+                                {ticketData.review !== '' && <DetailText label="리뷰" content={ticketData.review} />}
                             </T.DetailWrap>
                             <T.EditWrap>
                                 <span>수정하기</span>
