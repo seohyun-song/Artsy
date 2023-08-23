4d610cc2db9eecc68e1eb59755fda081c5a937b6
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-23 23:52:09 +0900

    Design: SplitLayout 제거

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,10 +1,9 @@
 import { useTheme } from 'styled-components';
 import * as S from './Signin.styles';
 import Input from '@components/@common/Input/Input';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useLoginQuery from '@hooks/@queries/useLoginQuery';
 import { useNavigate } from 'react-router-dom';
-import SplitLayout from '@components/SplitLayout/SplitLayout';
 import useWindowWidth from '@hooks/useWindowWidth';
 import IntroBox from '@components/introBox/IntroBox';
 import { ERROR_TYPE } from '@constants/serverErrorType';
@@ -68,53 +67,47 @@ const Signin = () => {
         navigate('../register');
     };
 
-    const leftBgColor = useMemo(
-        () => 'background: linear-gradient(180deg, rgba(105, 96, 204, 0.8) 0%, #554dab 100%)',
-        []
-    );
     return (
         <>
             <GlobalStyle />
-            <SplitLayout leftCss={leftBgColor}>
-                <S.SignInContainer>
-                    <IntroBox />
-                    <S.SignInContent>
-                        <S.loginTitle>{windowWidth > 1280 ? 'Welcome!' : 'Artsy'}</S.loginTitle>
-                        <S.loginForm onSubmit={handleSubmit}>
-                            <Input
-                                placeholder="이메일 주소를 입력하세요."
-                                id="email"
-                                name="email"
-                                inputRef={emailInputRef}
-                                onChange={handleChange}
-                                value={loginInfo.email}
-                                rounded
-                                isRequired
-                                isValid={true}
-                                inputWidth="100%"
-                            />
-                            <Input
-                                inputType="password"
-                                placeholder="비밀번호를 입력하세요."
-                                id="password"
-                                name="password"
-                                onChange={handleChange}
-                                value={loginInfo.password}
-                                rounded
-                                isRequired
-                                isValid={true}
-                                inputWidth="100%"
-                            />
-                            <S.SignInButton color={theme.colors.point1} size={'large'}>
-                                로그인
-                            </S.SignInButton>
-                        </S.loginForm>
-                        <S.RegisterButton size={'large'} onClick={handleRegister}>
-                            회원가입
-                        </S.RegisterButton>
-                    </S.SignInContent>
-                </S.SignInContainer>
-            </SplitLayout>
+            <S.SignInContainer>
+                <IntroBox />
+                <S.SignInContent>
+                    <S.loginTitle>{windowWidth > 1280 ? 'Welcome!' : 'Artsy'}</S.loginTitle>
+                    <S.loginForm onSubmit={handleSubmit}>
+                        <Input
+                            placeholder="이메일 주소를 입력하세요."
+                            id="email"
+                            name="email"
+                            inputRef={emailInputRef}
+                            onChange={handleChange}
+                            value={loginInfo.email}
+                            rounded
+                            isRequired
+                            isValid={true}
+                            inputWidth="100%"
+                        />
+                        <Input
+                            inputType="password"
+                            placeholder="비밀번호를 입력하세요."
+                            id="password"
+                            name="password"
+                            onChange={handleChange}
+                            value={loginInfo.password}
+                            rounded
+                            isRequired
+                            isValid={true}
+                            inputWidth="100%"
+                        />
+                        <S.SignInButton color={theme.colors.point1} size={'large'}>
+                            로그인
+                        </S.SignInButton>
+                    </S.loginForm>
+                    <S.RegisterButton size={'large'} onClick={handleRegister}>
+                        회원가입
+                    </S.RegisterButton>
+                </S.SignInContent>
+            </S.SignInContainer>
         </>
     );
 };
