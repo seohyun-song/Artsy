748dbb65d7bccae6dadb78ef14df5f8d2b4e7767
[SW5] 김지윤 <eliceyunva17@gmail.com>
2023-08-23 17:37:41 +0900

    style: 안쓰는 변수 정리

--- src/utils/formatTicketDate.js
+++ src/utils/formatTicketDate.js
@@ -1,9 +1,6 @@
 import { format } from 'date-fns';
-import { ko } from 'date-fns/locale';
 
 const formatTicketDate = (date) => {
-    const timeZone = 'Asia/Seoul';
-
     return format(date, 'yyyy-MM-dd HH:mm');
 };
 
