40b27afe2c9c5859f09688325517aa88b02ea782
seohyun <amanda94@naver.com>
2023-08-24 22:58:03 +0900

    design: 등급바 애니메이션 추가

--- src/components/@common/GradeBar/GradeBar.styles.js
+++ src/components/@common/GradeBar/GradeBar.styles.js
@@ -35,10 +35,19 @@ const StyledBar = styled.div`
         position: absolute;
         top: 0;
         left: 0;
-        width: ${({ $percent }) => $percent + '%'};
         height: 100%;
         border-radius: ${({ $height }) => ($height ? $height / 2 + 'rem' : '0.5rem')};
         background-color: ${({ theme, $grade }) => theme.gradeColor[$grade]};
+        animation: bars 1s 0.5s forwards ease-in;
+    }
+
+    @keyframes bars {
+        0% {
+            width: 0px;
+        }
+        100% {
+            width: ${({ $percent }) => $percent + '%'};
+        }
     }
 `;
 
