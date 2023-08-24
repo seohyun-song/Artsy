b2d1bfeac57d7aad5313ce41e2ab1c60d260ab84
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-24 15:43:09 +0900

    Feat: 로그인상태를 확인하는 쿼리 작성

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,33 +1,35 @@
 import axios from 'axios';
+import { ERROR_TYPE } from '@constants/serverErrorType';
 import { ERROR_MESSAGE } from '@constants/message';
 import useToastContext from '@hooks/useToastContext';
 
 const { VITE_API_URL } = import.meta.env;
 
-const config = {
-    timeout: 3000,
-    baseURL: VITE_API_URL,
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-    },
-};
+const authApi = () => {
+    const toast = useToastContext();
 
-const axiosAuthInstance = axios.create(config);
+    const config = {
+        timeout: 3000,
+        baseURL: VITE_API_URL,
+        withCredentials: true,
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+    };
+    const axiosAuthInstance = axios.create(config);
 
-axiosAuthInstance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        const toast = useToastContext();
+    axiosAuthInstance.interceptors.response.use(
+        (response) => response,
+        (error) => {
+            const type = error?.response?.data?.error?.type;
+            if (!type || type === ERROR_TYPE.INTERNAL_SERVER_ERROR) {
+                toast.show(ERROR_MESSAGE.unexpected);
+            }
 
-        const type = error?.response?.data?.error?.type;
-        if (!type || type === ERROR_TYPE.INTERNAL_SERVER_ERROR) {
-            toast.show(ERROR_MESSAGE.unexpected);
+            return Promise.reject(error);
         }
-
-        return Promise.reject(error);
-    }
-);
-
-export default axiosAuthInstance;
+    );
+    return axiosAuthInstance;
+};
+export default authApi;
--- src/hooks/@queries/useAuthQuery.js
+++ src/hooks/@queries/useAuthQuery.js
@@ -1,14 +1,23 @@
-import axiosAuthInstance from '@api/auth';
 import { useQuery } from 'react-query';
 
 import { QUERY_KEY } from '@hooks/@queries/useUserInfoQuery';
+import authApi from '@api/auth';
 
+// const get = async (path) => {
+//     const api = authApi();
+//     const response = await api.get(path, options);
+//     return response.data;
+// };
+
+//TODO: 로그인, 로그아웃에 따라 유저의 로그인상태값을 바꾸기
 const useAuthQuery = () => {
-    const fetcher = () => axiosAuthInstance.get(QUERY_KEY);
+    const api = authApi();
+
+    const fetcher = () => api.get(QUERY_KEY);
     const options = {
         retry: false,
     };
-    const query = useQuery([QUERY_KEY], fetcher, options);
+    const query = useQuery([QUERY_KEY + '/auth'], fetcher, options);
     return query;
 };
 
