ef8871cb38e298ddb8bd999c1256844d71461574
seohyun <amanda94@naver.com>
2023-08-24 11:13:13 +0900

    feat: 버튼 두개박스 추가

---
+++ src/components/@common/ButtonWrap/ButtonWrap.jsx
@@ -0,0 +1,6 @@
+import StyledButtonWrap from './ButtonWrap.styles';
+
+const ButtonWrap = ({ children }) => {
+    return <StyledButtonWrap>{children}</StyledButtonWrap>;
+};
+export default ButtonWrap;
