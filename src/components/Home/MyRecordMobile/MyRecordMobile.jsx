65b67260ab072b34da01d1af7c274a1476ad3a3c
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-25 19:00:46 +0900

    Feat: 모바일, 태블릿에서 기록함 이동 링크 추가

--- src/components/Home/MyRecordMobile/MyRecordMobile.jsx
+++ src/components/Home/MyRecordMobile/MyRecordMobile.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import * as M from './MyRecordWeb.styles';
-
+import * as M from './MyRecordMobile.styles';
+import bookUrl from '@assets/icons/icon-book.png';
+import ArrowUrl from '@assets/icons/icon-arrowLeft.png';
 const MyRecordMobile = () => {
-    return <div></div>;
+    return (
+        <M.Container>
+            <M.BookImage src={bookUrl} alt={'book'} />
+            <M.Content>
+                당신의 소중한 문화 기록을 확인해보세요!<M.Arrow src={ArrowUrl} alt={'left arrow'}></M.Arrow>
+            </M.Content>
+        </M.Container>
+    );
 };
 
 export default MyRecordMobile;
