29bf422be2d6bfdb3a109eeca717a3da6834b032
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-25 17:15:23 +0900

    Fix: userInfo훅을 import하는 경로 수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,11 @@ import Badge from '@components/Home/Badge/Badge';
 import Record from '@components/Home/Record/Record';
 import Greeting from '@components/Home/Greeting/Greeting';
 import * as H from './Home.styles';
-import useUserInfoQuery from '@hooks/@queries/useUserInfoQuery';
+import { useUserGetQuery } from '@hooks/@queries/useUserInfoQuery';
 import Loading from '@components/@common/Loading/Loading';
 
 const Home = () => {
-    const { data: userInfo, isLoading } = useUserInfoQuery();
+    const { data: userInfo, isLoading } = useUserGetQuery();
 
     if (isLoading) return <Loading />;
     const { displayName, totalTicket } = userInfo;
