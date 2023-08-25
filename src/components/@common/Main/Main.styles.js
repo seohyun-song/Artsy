5fbc413158366f078c1a8ac9d420d6233651b9f1
seohyun <amanda94@naver.com>
2023-08-25 21:52:32 +0900

    design: main 상단 간격 추가

--- src/components/@common/Main/Main.styles.js
+++ src/components/@common/Main/Main.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const StyledMain = styled.main`
     flex: 1 auto;
-    padding: 10rem 0 8rem;
+    padding: 14rem 0 8rem;
 `;
 
 export default StyledMain;
