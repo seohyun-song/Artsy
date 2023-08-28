3450c9a33563c9b50e7490076246b32cec67ebb7
[SW5] 김지윤 <eliceyunva17@gmail.com>
2023-08-29 06:55:18 +0900

    fix : 에러핸들링추가

--- src/pages/UserStats/UserStats.jsx
+++ src/pages/UserStats/UserStats.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as U from './UserStats.styles';
 import Container from '@components/@common/Container/Container';
 import Loading from '@components/@common/Loading/Loading';
@@ -6,6 +6,8 @@ import useTicketTotalQuery from '@hooks/@queries/useTicketTotalQuery';
 import StatsBox from '@components/UserStats/StatsBox/StatsBox';
 import CoinIconUrl from '@assets/icons/icon-coin.png';
 import CalendarIconUrl from '@assets/icons/icon-calendar.png';
+import { ERROR_TYPE } from '@constants/serverErrorType';
+import { ERROR_MESSAGE } from '@constants/message';
 
 const UserStats = () => {
     const { getTotalCount, getTotalPrice } = useTicketTotalQuery();
@@ -13,6 +15,30 @@ const UserStats = () => {
     const { data: priceData, isLoading: isPriceLoading, isError: isPriceError, error: priceError } = getTotalPrice();
 
     const month = new Date().getMonth() + 1;
+
+    useEffect(() => {
+        if (!isCountError) return;
+        if (isCountError) {
+            const errorType = countError?.response?.data?.error?.type;
+
+            switch (errorType) {
+                default:
+                    toast.show(ERROR_MESSAGE.defaultError);
+            }
+        }
+    }, [isCountError]);
+
+    useEffect(() => {
+        if (!isPriceError) return;
+        if (isPriceError) {
+            const errorType = priceError?.response?.data?.error?.type;
+
+            switch (errorType) {
+                default:
+                    toast.show(ERROR_MESSAGE.defaultError);
+            }
+        }
+    }, [isPriceError]);
     return (
         <>
             {(isCountLoading || isPriceLoading) && <Loading />}
