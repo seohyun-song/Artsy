2eb918411f388b112619e8911cdb97e256b6036a
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-29 12:08:49 +0900

    Refactor: useInput 사용

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,22 +1,27 @@
-import { useTheme } from 'styled-components';
+import { useEffect, useRef } from 'react';
 import * as S from './Signin.styles';
 import Input from '@components/@common/Input/Input';
-import { useEffect, useRef, useState } from 'react';
-import useLoginQuery from '@hooks/@queries/useLoginQuery';
-import { useNavigate } from 'react-router-dom';
-import useWindowWidth from '@hooks/useWindowWidth';
 import IntroBox from '@components/@common/introBox/IntroBox';
+import useLoginQuery from '@hooks/@queries/useLoginQuery';
+import { useTheme } from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import useToastContext from '@hooks/useToastContext';
+import useInput from '@hooks/useInput';
+import useWindowWidth from '@hooks/useWindowWidth';
+import checkValidation from '@utils/checkValidation';
 import { ERROR_TYPE } from '@constants/serverErrorType';
 import { ERROR_MESSAGE } from '@constants/message';
-import GlobalStyle from '@styles/GlobalStyles';
-import checkValidation from '@utils/checkValidation';
-import useToastContext from '@hooks/useToastContext';
 
 const Signin = () => {
-    const [loginInfo, setLoginInfo] = useState({
+    const {
+        input: loginInfo,
+        onChange: handleChange,
+        reset,
+    } = useInput({
         email: '',
         password: '',
     });
+
     const navigate = useNavigate();
     const { mutate, isSuccess, isError, error } = useLoginQuery();
     const windowWidth = useWindowWidth();
@@ -28,39 +33,27 @@ const Signin = () => {
         if (isSuccess) navigate('/home');
     }, [isSuccess]);
     useEffect(() => {
-        if (isError) {
-            const errorType = error.response?.data?.error.type;
-            switch (errorType) {
-                case ERROR_TYPE.INCORRECT_PASSWORD: {
-                    toast.show(ERROR_MESSAGE.incorrectEmailOrPassword);
-                    break;
-                }
-                case ERROR_TYPE.NOT_FOUND_EMAIL: {
-                    toast.show(ERROR_MESSAGE.incorrectEmailOrPassword);
-                    break;
-                }
-                default: {
-                    toast.show(ERROR_MESSAGE.defaultError);
-                }
-            }
+        if (!isError) {
+            return;
+        }
+
+        const errorType = error.response?.data?.error.type;
+        if (errorType === ERROR_TYPE.INCORRECT_PASSWORD || ERROR_TYPE.NOT_FOUND_EMAIL || ERROR_TYPE.INVALID_PARAM) {
+            toast.show(ERROR_MESSAGE.incorrectEmailOrPassword);
         }
     }, [isError]);
 
-    const handleChange = (e) => {
-        setLoginInfo((cur) => ({ ...cur, [e.target.name]: e.target.value }));
-    };
     const handleSubmit = async (e) => {
         e.preventDefault();
         const email = loginInfo.email;
         const isCorrectFormat = checkValidation({ email });
         if (!isCorrectFormat) {
-            setLoginInfo({ email: '', password: '' });
+            toast.show(ERROR_MESSAGE.incorrectEmailFormat);
             emailInputRef.current.focus();
-
             return;
         }
         mutate(loginInfo);
-        setLoginInfo({ email: '', password: '' });
+        reset();
         emailInputRef.current.focus();
     };
 
@@ -69,47 +62,44 @@ const Signin = () => {
     };
 
     return (
-        <>
-            <GlobalStyle />
-            <S.SignInContainer>
-                <IntroBox />
-                <S.SignInContent>
-                    <S.loginTitle>{windowWidth > labtopSize ? 'Welcome!' : 'Artsy'}</S.loginTitle>
-                    <S.loginForm onSubmit={handleSubmit}>
-                        <Input
-                            placeholder="이메일 주소를 입력하세요."
-                            id="email"
-                            name="email"
-                            inputRef={emailInputRef}
-                            onChange={handleChange}
-                            value={loginInfo.email}
-                            rounded
-                            isRequired
-                            isValid={true}
-                            inputWidth="100%"
-                        />
-                        <Input
-                            inputType="password"
-                            placeholder="비밀번호를 입력하세요."
-                            id="password"
-                            name="password"
-                            onChange={handleChange}
-                            value={loginInfo.password}
-                            rounded
-                            isRequired
-                            isValid={true}
-                            inputWidth="100%"
-                        />
-                        <S.SignInButton color={theme.colors.point1} size={'large'}>
-                            로그인
-                        </S.SignInButton>
-                    </S.loginForm>
-                    <S.RegisterButton size={'large'} onClick={handleRegister}>
-                        회원가입
-                    </S.RegisterButton>
-                </S.SignInContent>
-            </S.SignInContainer>
-        </>
+        <S.SignInContainer>
+            <IntroBox />
+            <S.SignInContent>
+                <S.loginTitle>{windowWidth > labtopSize ? 'Welcome!' : 'Artsy'}</S.loginTitle>
+                <S.loginForm onSubmit={handleSubmit}>
+                    <Input
+                        placeholder="이메일 주소를 입력하세요."
+                        id="email"
+                        name="email"
+                        inputRef={emailInputRef}
+                        onChange={handleChange}
+                        value={loginInfo.email}
+                        rounded
+                        isRequired
+                        isValid={true}
+                        inputWidth="100%"
+                    />
+                    <Input
+                        inputType="password"
+                        placeholder="비밀번호를 입력하세요."
+                        id="password"
+                        name="password"
+                        onChange={handleChange}
+                        value={loginInfo.password}
+                        rounded
+                        isRequired
+                        isValid={true}
+                        inputWidth="100%"
+                    />
+                    <S.SignInButton color={theme.colors.point1} size={'large'}>
+                        로그인
+                    </S.SignInButton>
+                </S.loginForm>
+                <S.RegisterButton size={'large'} onClick={handleRegister}>
+                    회원가입
+                </S.RegisterButton>
+            </S.SignInContent>
+        </S.SignInContainer>
     );
 };
 export default Signin;
