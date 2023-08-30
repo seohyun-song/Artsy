d9f6e3da86e307072830d732426da3a65462e19d
seohyun <amanda94@naver.com>
2023-08-30 19:30:28 +0900

    design: max-width 500px 컨테이너추가

--- src/components/@common/Wrap/Wrap.styles.js
+++ src/components/@common/Wrap/Wrap.styles.js
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 
 const StyledMaxWidth = styled.div`
-    max-width: ${({ theme }) => theme.devicesSize.labtop};
+    max-width: 50rem;
     margin: 0 auto;
-    padding: 0 2rem;
-    box-sizing: border-box;
 `;
 
 export default StyledMaxWidth;
