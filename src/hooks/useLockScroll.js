cf17a1c41b6b603cbd1f637cb0296de9c03897f6
[SW5] 이지영01~ <devluna0505@gmail.com>
2023-08-31 15:36:48 +0900

    Fix: 사이드바가 열릴때 스크롤바가 없어지면서 너비가 길어지는 오류 수정

--- src/hooks/useLockScroll.js
+++ src/hooks/useLockScroll.js
@@ -9,12 +9,14 @@ const useLockScroll = () => {
         document.body.style.position = 'fixed';
         document.body.style.top = `-${scrollY}px`;
         document.body.style.width = '100%';
+        document.body.style.overflowY = 'scroll';
     }, []);
     const unLockScroll = useCallback(() => {
         document.body.style.removeProperty('overflow');
         document.body.style.removeProperty('position');
         document.body.style.removeProperty('top');
         document.body.style.removeProperty('width');
+        document.body.style.removeProperty('overflowY');
         window.scrollTo(0, scrollPosition);
     }, [scrollPosition]);
 
